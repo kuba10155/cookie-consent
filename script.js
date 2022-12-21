3413393e1c794109499c707910ab0921a53c8e28
kuba10155 <j.zborowski24@gmail.com>
2022-12-21 14:30:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ consentForm.addEventListener('submit', function(event){
 
   modalText.innerHTML = `
     <div class="modal-inner-loading">
-      <img src="/images/loading.svg" class="loading">
+      <img src="loading.svg" class="loading">
       <p id="uploadText">
           Uploading your data to the dark web...
       </p>
@@ -41,7 +41,7 @@ consentForm.addEventListener('submit', function(event){
     <h2>Thanks <span class="modal-display-name">${fullName}</span>, you sucker! </h2>
     <p>We just sold the rights to your eternal soul.</p>
     <div class="idiot-gif">
-      <img src="/images/pirate.gif">
+      <img src="pirate.gif">
     </div>`
     modalCloseBtn.disabled = false
   }, 3000)
